773538e04053e5453e616df4b8f7a97d0dba9d77
bg117 <bg117@sooosilly.xyz>
2023-07-13 13:04:59 +0800

    Fix export spreadsheet

--- InventorySystem/ViewModels/ExportSpreadsheetViewModel.cs
+++ InventorySystem/ViewModels/ExportSpreadsheetViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Windows.Input;
 using OfficeOpenXml;
 using OfficeOpenXml.Table;
@@ -44,81 +45,84 @@ public class ExportSpreadsheetViewModel
 
         var inventorySheet = worksheets[instance.InventorySheetName] ?? worksheets.Add(instance.InventorySheetName);
         var inventoryTable = inventorySheet.Tables[instance.InventoryTableName];
+        var items = InventorySingletonViewModel.Instance.Items.Select(item => new object[]
+            { item.Id, item.Name, item.Description, item.Quantity, item.Price }).ToArray();
 
         if (inventoryTable == null)
         {
-            var table = inventorySheet.Tables.Add(inventorySheet.Cells[1, 1, 2, 5], instance.InventoryTableName);
-            var row = table.Range.Start.Row;
+            var tableRange = inventorySheet.Cells["A1:E2"];
+            var table = inventorySheet.Tables.Add(tableRange, instance.InventoryTableName);
 
             table.ShowHeader = true;
             table.TableStyle = TableStyles.Medium2;
             // add headers
-            inventorySheet.Cells[row, 1].Value = "Id";
-            inventorySheet.Cells[row, 2].Value = "Name";
-            inventorySheet.Cells[row, 3].Value = "Description";
-            inventorySheet.Cells[row, 4].Value = "Quantity";
-            inventorySheet.Cells[row, 5].Value = "Price";
+            tableRange.Value = new object[,]
+            {
+                { "ID", "Name", "Description", "Quantity", "Price" }
+            };
 
             inventoryTable = table;
             instance.InventoryTableName = table.Name;
         }
-
-        foreach (var item in InventorySingletonViewModel.Instance.Items)
+        else
         {
-            var row = inventoryTable.AddRow().Start.Row;
-
-            inventorySheet.Cells[row, 1].Value = item.Id;
-            inventorySheet.Cells[row, 2].Value = item.Name;
-            inventorySheet.Cells[row, 3].Value = item.Description;
-            inventorySheet.Cells[row, 4].Value = item.Quantity;
-            inventorySheet.Cells[row, 5].Value = item.Price;
+            inventoryTable.AddRow();
+            var tableRange = inventorySheet.Cells[inventoryTable.Address.Start.Row + 1,
+                inventoryTable.Address.Start.Column, inventoryTable.Address.End.Row - 1,
+                inventoryTable.Address.End.Column];
+            tableRange.Delete(eShiftTypeDelete.Up);
         }
+
+        for (var i = 0; i < items.Length - 1; i++)
+            inventoryTable.AddRow();
+        inventoryTable.Range.Offset(1, 0).LoadFromArrays(items);
     }
 
     private static void ExportTransactions(ExcelWorksheets worksheets, SpreadsheetSingletonViewModel instance)
     {
         instance.TransactionSheetName ??= "Transactions";
         instance.TransactionTableName ??= "Transactions";
-
         var transactionSheet =
             worksheets[instance.TransactionSheetName] ?? worksheets.Add(instance.TransactionSheetName);
         var transactionTable = transactionSheet.Tables[instance.TransactionTableName];
+        var transactions = TransactionsSingletonViewModel.Instance.Transactions.Select(transaction => new object[]
+        {
+            transaction.Id, transaction.Date, transaction.Item.Id, transaction.StockIn, transaction.StockOut,
+            transaction.Status, transaction.TotalPrice, transaction.Notes
+        }).ToArray();
 
         if (transactionTable == null)
         {
-            var table = transactionSheet.Tables.Add(transactionSheet.Cells[1, 1, 2, 8],
+            var tableRange = transactionSheet.Cells["A1:H2"];
+            var table = transactionSheet.Tables.Add(tableRange,
                 instance.TransactionTableName);
-            var row = table.Range.Start.Row;
 
             table.ShowHeader = true;
             table.TableStyle = TableStyles.Medium2;
 
             // add headers
-            transactionSheet.Cells[row, 1].Value = "Id";
-            transactionSheet.Cells[row, 2].Value = "Date";
-            transactionSheet.Cells[row, 3].Value = "Item Id";
-            transactionSheet.Cells[row, 4].Value = "Stock In";
-            transactionSheet.Cells[row, 5].Value = "Stock Out";
-            transactionSheet.Cells[row, 6].Value = "Status";
-            transactionSheet.Cells[row, 7].Value = "Total Price";
-            transactionSheet.Cells[row, 8].Value = "Notes";
+            tableRange.Value = new object[,]
+            {
+                {
+                    "Transaction ID", "Transaction Date", "Item ID", "Stock In", "Stock Out", "Status", "Total Price",
+                    "Notes"
+                }
+            };
 
             transactionTable = table;
             instance.TransactionTableName = table.Name;
         }
-
-        foreach (var transaction in TransactionsSingletonViewModel.Instance.Transactions)
+        else
         {
-            var row = transactionTable.AddRow().Start.Row;
-
-            transactionSheet.Cells[row, 1].Value = transaction.Id;
-            transactionSheet.Cells[row, 2].Value = transaction.Date;
-            transactionSheet.Cells[row, 3].Value = transaction.Item.Id;
-            transactionSheet.Cells[row, 4].Value = transaction.StockIn;
-            transactionSheet.Cells[row, 5].Value = transaction.StockOut;
-            transactionSheet.Cells[row, 6].Value = transaction.Status;
-            transactionSheet.Cells[row, 7].Value = transaction.TotalPrice;
-            transactionSheet.Cells[row, 8].Value = transaction.Notes;
+            transactionTable.AddRow();
+            var tableRange = transactionSheet.Cells[transactionTable.Address.Start.Row + 1,
+                transactionTable.Address.Start.Column, transactionTable.Address.End.Row - 1,
+                transactionTable.Address.End.Column];
+            tableRange.Delete(eShiftTypeDelete.Up);
         }
+
+        for (var i = 0; i < transactions.Length - 1; i++)
+            transactionTable.AddRow();
+        transactionTable.Range.Offset(1, 0).LoadFromArrays(transactions);
     }
 }
